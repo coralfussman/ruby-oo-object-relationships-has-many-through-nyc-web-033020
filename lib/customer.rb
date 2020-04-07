34a94c6ea477b9e1f4ca6420f8870a235ba41676
coralfussman <coralfussman@gmail.com>
2020-04-07 08:15:38 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 class Customer
-  
+attr_reader :name, :age
+@@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.filter do |meals| 
+            meals.customer == self
+        end
+    end
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+      end
+
+
 end
